41841e14d2e2ddf492c10f357cd9c670d6ff55ff
AndreiVentu <andrei.t.ventuneac@gmail.com>
2021-03-29 15:29:36 +0300

    2 text inputs and one button
    Am adaugat doua text input uri si un button pentru a se realiza testarea conexiunii cu baza de date

--- App.js
+++ App.js
@@ -1,21 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View, SafeAreaView, Button, Alert,TextInput} from 'react-native';
 
 export default function App() {
+  const [email, onChangeEmail] = React.useState("");
+  const [password, onChangePass] = React.useState("");
+ 
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+    <TextInput
+        style={styles.input}
+        onChangeText={onChangeEmail}
+        value={email}
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangePass}
+        value={password}
+      />
+      
+      <Button title='Login' style={styles.buttonlgn}></Button>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
+  
   },
+  input:{
+    borderWidth:1,
+    borderColor:'black',
+    padding:8,
+    width:200,
+  },
+  buttonlgn:{
+    width:500,
+    bottom:40,
+
+  },
+  
 });
