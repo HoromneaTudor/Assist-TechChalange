c39b662c523145df00359ab28d4c46f4be5f9ba5
HoromneaTudor <62078729+HoromneaTudor@users.noreply.github.com>
2021-04-03 23:56:54 +0300

    register backend

--- server/index.js
+++ server/index.js
@@ -1,78 +1,69 @@
-const express=require("express");
-const mysql=require("mysql");
-const cors=require("cors");
+const express = require("express");
+const mysql = require("mysql");
+const cors = require("cors");
 // depricated  const bodyParser=require("body-parser");
 
-const app=express();
+const app = express();
 
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 
-const PORT=3001;
+const PORT = 3001;
 
-const mySqlConection=mysql.createConnection({
-    host: "mysqlhoteldb.mysql.database.azure.com",
-    user: "ATLAS@mysqlhoteldb",
-    password: "Assistehotel2021",
-    database: "hotel_db",
-    ssl: true
+const mySqlConection = mysql.createConnection({
+  host: "mysqlhoteldb.mysql.database.azure.com",
+  user: "ATLAS@mysqlhoteldb",
+  password: "Assistehotel2021",
+  database: "hotel_db",
+  ssl: true,
 });
 
-app.post('/register',(req,res)=>{
+app.post("/register", (req, res) => {
+  const first_name = req.body.first_name;
+  const second_name = req.body.second_name;
+  const email = req.body.email;
+  const phone = req.body.phone;
+  const username = req.body.username;
+  const password = req.body.password;
 
-    const first_name=req.body.first_name;
-    const second_name=req.body.second_name;
-    const email=req.body.email;
-    const phone=req.body.phone;
-    const username=req.body.username;
-    const password=req.body.password;
+  const register_querry =
+    "INSERT INTO accounts (first_name,second_name,email,phone,role,password) VALUES (?,?,?,?,?,?)";
 
-
-    const register_querry="INSERT INTO accounts (first_name,second_name,email,phone,role,username,password) VALUES (?,?,?,?,?,?,?)";
-
-    mySqlConection.query(
-        register_querry,
-        [first_name,second_name,email,phone,1,username,password],
-        (err,result)=>{
-            console.log(err);
-    });
-
+  mySqlConection.query(
+    register_querry,
+    [first_name, second_name, email, phone, 1, password],
+    (err, result) => {
+      //console.log(err);
+      res.send(err);
+    }
+  );
 });
 
-app.post('/login',(req,res)=>{
+app.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
 
-    const email=req.body.email;
-    const password=req.body.password;
+  //console.log(email);
+  //console.log(password);
 
-    //console.log(email);
-    //console.log(password);
-
-    const login_querry="SELECT * FROM accounts WHERE email = ? AND password = ?";
-    mySqlConection.query(
-        login_querry,
-        [email,password],
-        (err,result)=>{
-            if(err)
-            {
-                res.send({err:err});
-            }
-            if(Object.keys(result).length > 0)
-            {
-                res.send(result);
-                //console.log(Object.keys(result).length);
-            }
-            else
-            {
-                //console.log("wrong");
-                //console.log(result);
-                res.send({message:"Wrong username/password combination"});
-            }
-        }
-    );
-
+  const login_querry =
+    "SELECT * FROM accounts WHERE email = ? AND password = ?";
+  mySqlConection.query(login_querry, [email, password], (err, result) => {
+    if (err) {
+      res.send({ err: err });
+    }
+    if (Object.keys(result).length > 0) {
+      res.send(result);
+      //console.log(Object.keys(result).length);
+    } else {
+      //console.log("wrong");
+      //console.log(result);
+      res.send({ message: "Wrong username/password combination" });
+    }
+  });
 });
 
-app.listen(process.env.PORT || PORT,()=>{
-    console.log('Running on port '+ PORT);
-})
+app.listen(process.env.PORT || PORT, () => {
+  console.log("Running on port " + PORT);
+});
